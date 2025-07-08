3e67e92ab3e473838a1e3adaf0898564ae44e160
Ali Raza <coderaliraza860@gmail.com>
2025-07-08 11:54:14 +0500

    Add files via upload

---
+++ 7)raw string/4)raw string.py
@@ -0,0 +1,14 @@
+#Escape Sequence 
+print("This is a line A \nThis is a line B")
+#Raw String 
+print(r"This is a Line A \n This is a line B")
+
+#Escape Sequence 
+print("\\\"")
+#Raw String
+print(r" \" ")
+
+#Esacpe Sequence
+print("\\\'")
+#Raw String
+print(r" \" ")
