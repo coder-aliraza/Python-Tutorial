686969ac02c1a7beccb8c234acef2ed0bc30cdbf
Ali Raza <coderaliraza860@gmail.com>
2025-07-08 11:48:27 +0500

    Add files via upload

---
+++ 2) Escape sequence/2)escape sequence.py
@@ -0,0 +1,29 @@
+# single quotes
+print('I\'m programmer')
+
+# Double Quotes
+print("Hello \"hello world\"world")
+print("I\"m Programmer")
+
+# backslash
+print("this is a \ Backslash")
+print("This is a Backslash\\")
+print("THis is a BackSlash\\\\")
+
+# New Line
+print("Line A")
+print("Line B")
+print("this is a line A \nthis is a line B") #if you add spaces after the \n, they will not be ignore, they will be included.
+
+# Carriage return
+print("Hello world")
+print("Hel \rlo   world")
+
+# Tab
+print("I'm \t Python")
+
+# Backspace
+print("hee\bllo \bPython")
+
+# form feed
+print("hel\flo \f world")
