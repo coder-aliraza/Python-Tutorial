4d5937991d9aa5a138b1bd235680d7a0391e0937
Ali Raza <coderaliraza860@gmail.com>
2025-07-08 11:52:33 +0500

    Add files via upload

---
+++ 3) Comments/3) Comments.py
@@ -0,0 +1,16 @@
+# This is a single-line comment
+print("Hello, world!")  # This comment is next to a line of code
+# print("Hello world")  
+#First select this line which you want comment.Then Press (ctrl + /) key.
+
+
+
+#Multi-line comment
+"""
+print("This is a line A")
+print("This is a line B")
+"""
+'''
+print("This is a line A")
+print("This is a line B")
+'''
