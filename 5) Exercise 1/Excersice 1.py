892a486ea0927fb2f1a5495953258a5c5d912c41
Ali Raza <coderaliraza860@gmail.com>
2025-07-08 11:53:45 +0500

    Add files via upload

---
+++ 5) Exercise 1/Excersice 1.py
@@ -0,0 +1,5 @@
+# --- print these following lines---
+# This is a \\ Double backslash
+# This is a /\/\/\/\/\ mountain
+# He is a         Awesome(Use escape sequence instead of manual spaces)
+# \" \n \t \' (Print this as an output)
