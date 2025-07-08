e9dc36ac4fe73ef23687719341d96e9d76d1a646
Ali Raza <coderaliraza860@gmail.com>
2025-07-08 11:53:27 +0500

    Add files via upload

---
+++ 4)escape sequence as a normal text/4)escape sequence as a normal text.py
@@ -0,0 +1,13 @@
+# Esacpe Sequence
+print("This is a Line A \n This is a line B")
+#Escape Sequence as a noraml text treat.
+print("This is a Line A \\n This is a line B") 
+
+# Esacpe Sequence
+print("This is a \t tab")
+#Escape Sequence as a noraml text treat.
+print("This is a \\t tab")
+
+
+# Solve this Exercise line number 13. Output this screen.
+# output: \" \' 
