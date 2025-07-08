621a90a14fdff2ff84402f148e8dda8e092a6cdb
Ali Raza <coderaliraza860@gmail.com>
2025-07-08 11:48:11 +0500

    Add files via upload

---
+++ 1) print Function/1)print function.py
@@ -0,0 +1,10 @@
+print("hello world")
+print('hello world')
+print('Hello "World" python')
+print("Helo 'World' python")
+print("I'm Programmer")
+
+
+# print("hello "word" Python") syntax error
+# print('hello 'world' Python') syntax error
+ 
